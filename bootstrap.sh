caed51da58df492a1793b7e95ada618c13efa7d1
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2019-09-26 14:30:18 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,38 @@
+#!/usr/bin/env bash
+
+
+#########################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
+# Updated by bunbunbunbunbunny for crypto use...
+# This script is intended to be run like this:
+#
+#   curl https://raw.githubusercontent.com/bunbunbunbunbunny/Multi-Pool-Installer/master/bootstrap.sh | bash
+#
+#########################################################
+if [ -z "${TAG}" ]; then
+	TAG=v1.68
+fi
+
+
+# Clone the MultiPool repository if it doesn't exist.
+if [ ! -d $HOME/multipool ]; then
+	if [ ! -f /usr/bin/git ]; then
+		echo Installing git . . .
+		apt-get -q -q update
+		DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
+		echo
+	fi
+
+	echo Downloading bunbunbunbunbunny MultiPool Installer ${TAG}. . .
+	git clone \
+		-b ${TAG} --depth 1 \
+		https://github.com/bunbunbunbunbunny/multipool_setup \
+		"$HOME"/multipool/install \
+		< /dev/null 2> /dev/null
+
+	echo
+fi
+
 # Set permission and change directory to it.
 cd $HOME/multipool/install
 
