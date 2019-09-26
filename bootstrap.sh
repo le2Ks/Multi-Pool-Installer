c4f8807e924cbf806e32e90b9c1d6d8a470c2de3
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2019-09-26 14:25:40 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+
+
+#########################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
+# Updated by bunbunbunbunbunny for crypto use...
+# This script is intended to be run like this:
+#
+#   curl https://raw.githubusercontent.com/bunbunbunbunbunny/Multi-Pool-Installer/master/bootstrap.sh | bash
+#
+#########################################################
 if [ -z "${TAG}" ]; then
 	TAG=v1.68
 fi
