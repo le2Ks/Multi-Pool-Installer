41e44a91021932af293cce1ef2b3c6c7d019706a
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2019-09-26 14:28:17 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,3 @@
-if [ -z "${TAG}" ]; then
-	TAG=v1.68
-fi
-
-
-# Clone the MultiPool repository if it doesn't exist.
-if [ ! -d $HOME/multipool ]; then
-	if [ ! -f /usr/bin/git ]; then
-		echo Installing git . . .
-		apt-get -q -q update
-		DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
-		echo
-	fi
-
-	echo Downloading bunbunbunbunbunny MultiPool Installer ${TAG}. . .
-	git clone \
-		-b ${TAG} --depth 1 \
-		https://github.com/bunbunbunbunbunny/multipool_setup \
-		"$HOME"/multipool/install \
-		< /dev/null 2> /dev/null
-
-	echo
-fi
-
 # Set permission and change directory to it.
 cd $HOME/multipool/install
 
