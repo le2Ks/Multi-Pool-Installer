6b085eb39859034ae547e4aa6c976e041fe79816
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2019-09-26 17:11:10 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,44 +9,28 @@
 #   curl https://raw.githubusercontent.com/bunbunbunbunbunny/Multi-Pool-Installer/master/bootstrap.sh | bash
 #
 #########################################################
-if [ -z "${TAG}" ]; then
-	TAG=v1.68
-fi
-
 
 # Clone the MultiPool repository if it doesn't exist.
-if [ ! -d $HOME/multipool ]; then
-	if [ ! -f /usr/bin/git ]; then
 		echo Installing git . . .
 		apt-get -q -q update
 		DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
-		echo
-	fi
 
+	
 	echo Downloading bunbunbunbunbunny MultiPool Installer ${TAG}. . .
-	git clone \
-		-b ${TAG} --depth 1 \
-		https://github.com/bunbunbunbunbunny/multipool_setup \
-		"$HOME"/multipool/install \
-		< /dev/null 2> /dev/null
 
-	echo
-fi
-
-# Set permission and change directory to it.
-cd $HOME/multipool/install
-
-# Update it.
-sudo chown -R $USER $HOME/multipool/install/.git/
-if [ "${TAG}" != `git describe --tags` ]; then
-	echo Updating bunbunbunbunbunny MultiPool Installer to ${TAG} . . .
-	git fetch --depth 1 --force --prune origin tag ${TAG}
-	if ! git checkout -q ${TAG}; then
-		echo "Update failed. Did you modify something in `pwd`?"
-		exit
-	fi
-	echo
-fi
+	git clone https://github.com/bunbunbunbunbunny/multipool_setup \
+	mkdir multipool
+	cd multipool
+	mkdir install
+	cd 
+	sudo chmod 775 -R multipool
+	git clone https://github.com/bunbunbunbunbunny/multipool_setup.git
+	sudo chmod 775 -R multipool_setup
+	cd multipool_setup
+	./start.sh
+	cd
+	
+		
 
 # Start setup script.
 bash $HOME/multipool/install/start.sh
