9b365fd0a4c103a47d5e6616eb283ec270f7818d
gnark-mining <46746362+gnark-mining@users.noreply.github.com>
2020-03-26 12:14:43 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,29 +3,50 @@
 
 #########################################################
 # Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
-# Updated by bunbunbunbunbunny for crypto use...
+# Updated by cryptopool.builders for crypto use...
 # This script is intended to be run like this:
 #
-#   curl https://raw.githubusercontent.com/bunbunbunbunbunny/Multi-Pool-Installer/master/bootstrap.sh | bash
+#   curl https://raw.githubusercontent.com/cryptopool-builders/Multi-Pool-Installer/master/bootstrap.sh | bash
 #
 #########################################################
+if [ -z "${TAG}" ]; then
+	TAG=v1.68
+fi
 
 
-	
-	echo Downloading bunbunbunbunbunny MultiPool Installer ${TAG}. . .
-	
-	apt-get install git;
-	mkdir multipool;
-	cd multipool;
-	mkdir install;
-	cd;
-	git clone https://github.com/bunbunbunbunbunny/multipool_setup;
-	
-clear
-	echo Copying files ${TAG}. . . 
-	
-	
-	cp -a multipool_setup/. multipool/install/;
-	chmod 775 -R multipool;
-	bash multipool/install/start.sh;
-	
+# Clone the MultiPool repository if it doesn't exist.
+if [ ! -d $HOME/multipool ]; then
+	if [ ! -f /usr/bin/git ]; then
+		echo Installing git . . .
+		apt-get -q -q update
+		DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
+		echo
+	fi
+
+	echo Downloading MultiPool Installer ${TAG}. . .
+	git clone \
+		-b ${TAG} --depth 1 \
+		https://github.com/gnark-mining/multipool_setup \
+		"$HOME"/multipool/install \
+		< /dev/null 2> /dev/null
+
+	echo
+fi
+
+# Set permission and change directory to it.
+cd $HOME/multipool/install
+
+# Update it.
+sudo chown -R $USER $HOME/multipool/install/.git/
+if [ "${TAG}" != `git describe --tags` ]; then
+	echo Updating MultiPool Installer to ${TAG} . . .
+	git fetch --depth 1 --force --prune origin tag ${TAG}
+	if ! git checkout -q ${TAG}; then
+		echo "Update failed. Did you modify something in `pwd`?"
+		exit
+	fi
+	echo
+fi
+
+# Start setup script.
+bash $HOME/multipool/install/start.sh
